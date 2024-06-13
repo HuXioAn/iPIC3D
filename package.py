caa1a38a13e6d6f3fb041e12570fa5c6158733d3
HuXioan <hu7006@outlook.com>
2024-06-13 15:29:25 +0200

    update dependancy hdf5_hl to spack

--- package.py
+++ package.py
@@ -27,10 +27,9 @@ class Testspack(CMakePackage):
     """ iPIC3D
         An implicit PIC simulator for plasma physics...
     """
-
     # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://github.com/HuXioAn/iPIC3D"
-    url = "https://github.com/HuXioAn/iPIC3D/archive/refs/tags/trv1.tar.gz"
+    url = "https://github.com/HuXioAn/iPIC3D/archive/refs/tags/spack2.tar.gz"
 
     # FIXME: Add a list of GitHub accounts to
     # notify when the package is updated.
@@ -41,12 +40,13 @@ class Testspack(CMakePackage):
     # the license, set checked_by to your Github username.
     #license("UNKNOWN", checked_by="github_user1")
 
-    version("1", sha256="d1ed7b4d53938075b01480d77ec4f02960b5ef9cf865423e6a9d2b87a70c5ad3")
+    #version("1", sha256="d1ed7b4d53938075b01480d77ec4f02960b5ef9cf865423e6a9d2b87a70c5ad3")
+    version("2", sha256="64ca8f84ecc6d02dcab32cf278d46cea4b8ad281158de67fc820a10667d2dbd1")
 
     # FIXME: Add dependencies if required.
     depends_on("mpich")
     depends_on("hdf5")
-
+    depends_on("hdf5+hl")
     def cmake_args(self):
         # FIXME: Add arguments other than
         # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
